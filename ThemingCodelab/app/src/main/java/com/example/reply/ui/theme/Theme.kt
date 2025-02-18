505bdb06992414209045ad28fb6c713bd8605488
Sifanurfaizah <sifa.nur@student.umn.ac.id>
2025-02-18 15:39:23 +0700

    5. Menambahkan warna dinamis di aplikasi

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -72,13 +72,27 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun AppTheme(
-    useDarkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    useDarkTheme: Boolean =  isSystemInDarkTheme(),
+    content: @Composable () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = useDarkTheme
+        }
     }
 
     MaterialTheme(
